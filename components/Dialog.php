ce142aba74748b6ece39669e7c8e148df0a7a35f
AngeloDmrch <angelodmrch@gmail.com>
2021-02-15 08:52:33 -0300

    First commit

---
+++ components/Dialog.php
@@ -0,0 +1,19 @@
+<?php namespace Dmrch\Lgpd\Components;
+
+use Cms\Classes\ComponentBase;
+
+class Dialog extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'Dialog Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+}
