03f7b9a9f86345386293ee9a7769cfad38818a3e
AngeloDmrch <angelodmrch@gmail.com>
2021-06-11 10:42:30 -0300

    add new text field

--- components/Dialog.php
+++ components/Dialog.php
@@ -1,15 +1,16 @@
 <?php namespace Dmrch\Lgpd\Components;
 
 use Cms\Classes\ComponentBase;
+use Backend\Models\BrandSetting;
 use Dmrch\Lgpd\Models\Settings;
 
 class Dialog extends ComponentBase
 {
-
     public $text;
     public $btn_label;
     public $css;
-
+    public $politica_text;
+    public $app_name;
 
     public function componentDetails()
     {
@@ -28,5 +29,7 @@ class Dialog extends ComponentBase
         $this->text = Settings::get('text');
         $this->btn_label = Settings::get('btn_label');
         $this->css = Settings::get('css');
+        $this->politica_text = Settings::get('politica_text');
+        $this->app_name = BrandSetting::get('app_name');
     }
 }
